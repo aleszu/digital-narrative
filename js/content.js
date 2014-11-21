372e75460b172e66bc5068294136ecb22729c507
Alex Johnson <alex@tablelist.com>
2014-11-21 10:44:56 -0500

    Load data from content.js JSON file. Iterate through and render content

---
+++ js/content.js
@@ -0,0 +1,89 @@
+
+// Section = { id: "", title : "", image: "" }
+var sections = [{ 
+	id: "a", title : "Title 01", image: "", sentences : 12 
+},{ id: "b", title : "Title 02", image: "", sentences : 8 }]
+
+// Sentence = { id: "", sentence : "", link, "" }
+// 100 Words or 700 characters per section
+var sentences = [{
+	id: "a001",
+	sentence: "There is only one way to understand the 1,933-mile line that divides our country from Mexico.",
+	link: "",
+}, {
+	id: "a002",
+	sentence: "Start at the beach and walk east until you hit the Gulf.",
+	link: "",
+}, {
+	id: "a003",
+	sentence: "The fence starts about eighty feet out into the Pacific and is made of metal pylons.",
+	link: "",
+}, {
+	id: "a004",
+	sentence: "Once it hits dry land, it goes up the beach and onto a little hill.",
+	link: "",
+}, {
+	id: "a005",
+	sentence: "There it becomes two fences and looks more like a prison barrier.",
+	link: "",
+}, {
+	id: "a006",
+	sentence: "One fence follows the border line and the other runs parallel, north of it.",
+	link: "",
+}, {
+	id: "a007",
+	sentence: "The buffer zone between the two is reserved for the US Border control, except for when, on Saturdays and Sundays citizens are allowed to enter to look at and talk to Mexicans on the other side.",
+	link: "",
+}, {
+	id: "a008",
+	sentence: "However, they are not allowed physical contact with them.",
+	link: "",
+}, {
+	id: "a009",
+	sentence: "This place is called the “Friendship Circle” and a marble obelisk stands in its center, breaking the fence.",
+	link: "",
+}, {
+	id: "a010",
+	sentence: "The obelisk was placed in 1851 by the International Boundary Commission, after the end of the Mexican-American War, when Mexico surrendered over half its land to the US.",
+	link: "",
+}, {
+	id: "a011",
+	sentence: "The International Boundary Commission started on the beachfront hill when revising the border between the two countries and the obelisk was their first survey marker.",
+	link: "",
+}, {
+	id: "a012",
+	sentence: "",
+	link: "",
+}, {
+	id: "b001",
+	sentence: "In 1851, some men from something called the International Boundary Commission placed the obelisk here.",
+	link: "",
+}, {
+	id: "b002",
+	sentence: "Back then, the Mexican-American War had just ended, and Mexico had agreed to surrender more than half its territory to the United States, including the places now called California, Arizona, New Mexico, and Texas.",
+	link: "",
+}, {
+	id: "b003",
+	sentence: "The job of the International Boundary Commission was to come up with a map of the revised frontier between the two countries.",
+	link: "",
+}, {
+	id: "b004",
+	sentence: "They started here, on this beachfront hill, and installed the obelisk as their first survey marker.",
+	link: "",
+}, {
+	id: "b005",
+	sentence: "Then they walked east, into the borderlands, on a dangerous journey.",
+	link: "",
+}, {
+	id: "b006",
+	sentence: "They had only two choices of transportation – by foot or by horse.",
+	link: "",
+}, {
+	id: "b007",
+	sentence: "Today you can fly or drive in just a short amount of time.",
+	link: "",
+}, {
+	id: "b008",
+	sentence: "But if you really want to understand something, sometimes you have to slow down.",
+	link: "",
+}];
